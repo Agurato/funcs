2c42e75f34e50c4482521d4f66acd09d5d2e5e3a
Agurato <Agurato@users.noreply.github.com>
2024-01-05 11:23:39 +0100

    set discord cron

--- api/discord_cron.go
+++ api/discord_cron.go
@@ -10,16 +10,24 @@ import (
 )
 
 func DiscordCronHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Authorization") != fmt.Sprintf("Bearer %s", os.Getenv("CRON_SECRET")) {
+		slog.Error("unauthorized")
+		w.WriteHeader(http.StatusUnauthorized)
+	}
+
 	client, err := webhook.NewWithURL(os.Getenv("DISCORD_WEBHOOK_URL"))
 	if err != nil {
-		slog.Error("can't init webhook client", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write([]byte(fmt.Sprintf("can't init webhook client: %v", err)))
 		return
 	}
 
 	msg, err := client.CreateContent("hello world")
 	if err != nil {
-		slog.Error("can't init webhook client", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write([]byte(fmt.Sprintf("can't create content: %v", err)))
 		return
 	}
-	slog.Info(fmt.Sprintf("sent msg id %s", msg.ID))
+	slog.Debug(fmt.Sprintf("sent msg id %s", msg.ID))
+	w.WriteHeader(http.StatusOK)
 }
