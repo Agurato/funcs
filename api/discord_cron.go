bc9eeca9277d4a4c5b80d080094312064f9f9868
Agurato <Agurato@users.noreply.github.com>
2024-01-05 11:34:14 +0100

    fix authorization

--- api/discord_cron.go
+++ api/discord_cron.go
@@ -11,21 +11,22 @@ import (
 
 func DiscordCronHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Header.Get("Authorization") != fmt.Sprintf("Bearer %s", os.Getenv("CRON_SECRET")) {
-		slog.Error("unauthorized")
+		_, _ = w.Write([]byte("unauthorized"))
 		w.WriteHeader(http.StatusUnauthorized)
+		return
 	}
 
 	client, err := webhook.NewWithURL(os.Getenv("DISCORD_WEBHOOK_URL"))
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
 		_, _ = w.Write([]byte(fmt.Sprintf("can't init webhook client: %v", err)))
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
 	msg, err := client.CreateContent("hello world")
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
 		_, _ = w.Write([]byte(fmt.Sprintf("can't create content: %v", err)))
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	slog.Debug(fmt.Sprintf("sent msg id %s", msg.ID))
